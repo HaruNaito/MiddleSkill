41a715ac522e9673f09a44f54df25a66ff6fc4dc
HaruNaito <112502762+HaruNaito@users.noreply.github.com>
2023-04-23 08:13:58 +0900

    1-18

---
+++ 1-18/src/test1/Getter.java
@@ -0,0 +1,14 @@
+package test1;
+
+
+public class Getter{
+	private String sample;
+
+	public String getSample() {
+		return sample;
+	}
+
+	public void setSample(String sample) {
+		this.sample = sample;
+	}
+}
